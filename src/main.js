c04e68de869028087e73f071a5d8d7d72e82d088
何灏 <hehao@userdeMBP.Bitman's\032Private\032Network>
2015-07-29 16:33:14 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 
-console.log("hello");
+(
+    function(){
+for (var i=1;i<=20;i++) {
+
+    console.log(i);
+}
+}
+    )();
+(
+    function(){
+        for (var i=1;i<=20;i=i+2){
+            console.log(i)
+        }
+    }
+)();
